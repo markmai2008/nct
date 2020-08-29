00141f7702f602779e788732e0a62847030d8da3
markmai2008 <3263549762@qq.com>
2020-08-29 09:57:48 +0800

    p_0_1

---
+++ p_0_1.py
@@ -0,0 +1,11 @@
+""" P5
+"""
+a = int(input("输入一个正整数："))
+b = int(input("输入第二个正整数："))
+print(abs(a-b))
+# if a > b:
+#     print(a - b)
+# else:
+#     print(b - a)
+# print(a-b if a>b else b-a)
+print(a + b)
